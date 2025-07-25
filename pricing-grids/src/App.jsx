39ea38e45766245deb98f6ced4981658f7fcd699
PedroDelgo <pedrodelg.go@gmail.com>
2025-07-25 14:58:32 -0300

    feat: Creates cards hover animation and implements all 3 cards in the view.

--- pricing-grids/src/App.jsx
+++ pricing-grids/src/App.jsx
@@ -1,10 +1,15 @@
 import PricingCard from "./components/Pricing-card"
+
 function App() {
   return (
     <>
-      <div className="w-screen h-screen bg-slate-800 flex flex-col md:flex-row items-center justify-center">
-        <PricingCard/>
+    <div className="w-full h-screen bg-slate-800">
+      <div className="px-2 pt-10 pb-10 w-auto min-h-screen bg-slate-800 flex flex-col md:flex-row items-center justify-center space-y-5 md:space-y-0 md:space-x-5">
+        <PricingCard />
+        <PricingCard />
+        <PricingCard />
       </div>
+    </div>
     </>
   )
 }
--- pricing-grids/src/components/Pricing-card.jsx
+++ pricing-grids/src/components/Pricing-card.jsx
@@ -1,18 +1,27 @@
 function PricingCard() {
   return (
     <>
-      <div className="bg-zinc-600 rounded-xl felx flex-col items-center justify-center w-80 h-124 ">
-        <div className="bg-slate-800 h-118 flex flex-col justify-center-items-center rounded-xl m-3 p-5 space-y-2 ">
-          <h2 className=" bg-black h-15 flex items-center justify-center text-center text-white text-lg font-medium ">BASIC</h2>
-          <h1 className=" bg-black h-15 flex items-center  justify-center text-center text-white text-5xl font-bold" >100GB</h1>
-          <p className= " bg-black h-15 flex items-center justify-center text-center text-white text-sm font-light " >4.99$ per month</p>
-          <button className="bg-black h-15 flex items-center justify-center text-center text-white text-lg font-medium">
-            purchase
-          </button>
+      <div className="bg-zinc-600 hover:bg-violet-800 duration-500 rounded-xl felx flex-col items-center justify-center w-75 h-124 select-none">
+        <div className="bg-slate-800 h-118 flex flex-col justify-center-items-center rounded-xl m-3 p-5 space-y-2">
+          <h2 className="h-15 flex items-center justify-center text-center text-white text-lg font-medium ">BASIC</h2>
+          <h1 className="h-15 flex items-center  justify-center text-center text-white text-5xl font-bold" >100GB</h1>
+          <p className="h-15 flex items-center justify-center text-center text-white text-sm font-light " >4.99$ per month</p>
+          <div className="h-15 flex items-center justify-center text-center text-white text-lg font-medium" >
+            <button className="hover:bg-violet-800 hover:border-violet-800 duration-300 cursor-pointer border-2 w-35 rounded-xl h-13 flex items-center justify-center text-center text-white text-lg font-medium">
+              Purchase
+            </button>
+          </div>
+          <div className="border-t-1 mt-5 border-zinc-600 h-40 flex flex-col items-center space-y-5 justify-center text-center text-white text-sm font-medium ">
+            <p>100 gb storage</p>
+            <p>Option to add members</p>
+            <p>Extra members benefits</p>
+          </div>
         </div>
       </div>
     </>
   )
 }
 export default PricingCard
+
+
 
