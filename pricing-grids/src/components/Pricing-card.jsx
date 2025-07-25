2433ebef97add37a26913382846a8e085670288b
PedroDelgo <pedrodelg.go@gmail.com>
2025-07-25 15:06:24 -0300

    style: change intensity of violet color in the hover animation.

--- pricing-grids/src/components/Pricing-card.jsx
+++ pricing-grids/src/components/Pricing-card.jsx
@@ -1,13 +1,13 @@
 function PricingCard() {
   return (
     <>
-      <div className="bg-zinc-600 hover:bg-violet-800 duration-500 rounded-xl felx flex-col items-center justify-center w-75 h-124 select-none">
+      <div className="bg-zinc-600 hover:bg-violet-700 duration-500 rounded-xl felx flex-col items-center justify-center w-75 h-124 select-none">
         <div className="bg-slate-800 h-118 flex flex-col justify-center-items-center rounded-xl m-3 p-5 space-y-2">
           <h2 className="h-15 flex items-center justify-center text-center text-white text-lg font-medium ">BASIC</h2>
           <h1 className="h-15 flex items-center  justify-center text-center text-white text-5xl font-bold" >100GB</h1>
           <p className="h-15 flex items-center justify-center text-center text-white text-sm font-light " >4.99$ per month</p>
           <div className="h-15 flex items-center justify-center text-center text-white text-lg font-medium" >
-            <button className="hover:bg-violet-800 hover:border-violet-800 duration-300 cursor-pointer border-2 w-35 rounded-xl h-13 flex items-center justify-center text-center text-white text-lg font-medium">
+            <button className="hover:bg-violet-900 hover:border-violet-900 duration-300 cursor-pointer border-2 w-35 rounded-xl h-13 flex items-center justify-center text-center text-white text-lg font-medium">
               Purchase
             </button>
           </div>
