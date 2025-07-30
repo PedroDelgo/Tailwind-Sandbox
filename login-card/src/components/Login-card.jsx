4aaa60f19649613d63fcb038dc8201ab49d60ec6
PedroDelgo <pedrodelg.go@gmail.com>
2025-07-30 16:17:03 -0300

    style: change the font weight in desktop responsivity.

--- login-card/src/components/Login-card.jsx
+++ login-card/src/components/Login-card.jsx
@@ -8,13 +8,13 @@ function LoginCard() {
             <div className="bg-[#f9f9f9] w-100 h-160 md:w-250 rounded-md drop-shadow-2xl flex flex-col justify-center items-center md:justify-start md:flex-row select-none">
                 <div className='md:flex md:flex-col space-y-10 p-5 w-full h-full'>
                     <div className=" w-full p-2 space-y-6  flex flex-col md:items-start items-center justify-center">
-                        <p className='text-3xl font-bold'>Log in</p>
-                        <p>Log in to your account to upload or download pictures, videos or music</p>
+                        <p className='text-3xl md:text-5xl font-bold'>Log in</p>
+                        <p className='md:text-lg md:font-light'>Log in to your account to upload or download pictures, videos or music</p>
                     </div>
-                    <div className='w-full flex justify-center items-center'>
+                    <div className='w-full flex justify-center items-center md:justify-start'>
                         <input type="text"
                             placeholder='Type you email'
-                            className='border-1 border-slate-300 pl-5 text-start h-19 rounded-md w-80 md:h-17 md:w-122' />
+                            className='border-1 border-slate-300 pl-5 text-start h-19 rounded-md w-80  md:h-17 md:w-130' />
                     </div>
                     <div className='border-b-2 md:h-30 border-slate-300 w-full h-23 flex flex-row justify-center items-center space-x-9 md:space-x-30 pb-5'>
                         <p className=' cursor-pointer hover:text-blue-500 duration-200 text-gray-400'>Forgot your password?</p>
@@ -22,7 +22,7 @@ function LoginCard() {
                     </div>
                     <div className='w-full flex flex-col justify-center items-center p-5 space-y-10'>
                         <div>
-                            <p>Or login with</p>
+                            <p className='md:font-light'>Or login with</p>
                         </div>
                         <div className='space-x-10 md:space-x-28 w-full flex flex-row'>
                             <button className=' hover:bg-slate-200 md:w-50 duration-200 border-1 border-slate-300 pl-7 h-15 flex flex-row justify-center items-center space-x-2 w-40 p-2 rounded-md text-white font-bold'>
@@ -30,7 +30,7 @@ function LoginCard() {
                                     alt="icone do github"
                                     className='h-8 ml-3' />
                             </button>
-                            <button className=' hover:bg-slate-200 md:w-50 duration-200 border-1 border-slate-300 pl-7 h-15 flex flex-row justify-center items-center space-x-2 w-40 p-2 rounded-md text-white font-bold'>
+                            <button className=' hover:bg-slate-200 md:w-50 duration-200 border-1 border-slate-300 pl-7 h-15 flex flex-row justify-center items-center space-x-2 w-40 p-2 md:mr-1 rounded-md text-white font-bold'>
                                 <p className='text-black font-medium'>Github</p> <img src={githubIco}
                                     alt="icone do github"
                                     className='h-10' />
